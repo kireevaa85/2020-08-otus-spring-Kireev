7c65f69bc033a67fe22e666d370a9d1902da2383
kireevaa85 <kireevaa85@gmail.com>
2020-09-16 01:29:32 +0300

    hw03 Spring Boot Magic. PR part 5

--- hw03-springboot/src/main/java/ru/otus/dao/TicketDaoCsv.java
+++ hw03-springboot/src/main/java/ru/otus/dao/TicketDaoCsv.java
@@ -3,7 +3,6 @@ package ru.otus.dao;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
-import ru.otus.config.AppConfig;
 import ru.otus.config.TicketConfig;
 import ru.otus.domain.Question;
 import ru.otus.domain.Ticket;
@@ -21,13 +20,10 @@ import java.util.List;
 public class TicketDaoCsv implements TicketDao {
 
     private final TicketConfig ticketConfig;
-    private final AppConfig appConfig;
 
     @Override
     public Ticket getTicket() throws TicketNotFoundException {
-        final String resourceName = String.format("/%s_%s.%s",
-                ticketConfig.getResourceName(), appConfig.getLocale(), ticketConfig.getResourceExtension());
-        try (InputStream i = TicketDaoCsv.class.getResourceAsStream(resourceName);
+        try (InputStream i = TicketDaoCsv.class.getResourceAsStream(ticketConfig.getResourceName());
              BufferedReader r = new BufferedReader(new InputStreamReader(i))) {
             List<Question> questions = new ArrayList<>();
             String l;
@@ -44,7 +40,7 @@ public class TicketDaoCsv implements TicketDao {
             return new Ticket(questions);
         } catch (Exception e) {
             log.error(e.getMessage());
-            throw new TicketNotFoundException("Ticket with name " + resourceName + " not found");
+            throw new TicketNotFoundException("Ticket with name " + ticketConfig.getResourceName() + " not found");
         }
     }
 
--- hw03-springboot/src/test/java/ru/otus/dao/TicketDaoCsvTest.java
+++ hw03-springboot/src/test/java/ru/otus/dao/TicketDaoCsvTest.java
@@ -7,14 +7,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ru.otus.config.AppConfig;
 import ru.otus.config.TicketConfig;
 import ru.otus.domain.Ticket;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -27,17 +25,13 @@ class TicketDaoCsvTest {
 
     @Mock
     private TicketConfig ticketConfig;
-    @Mock
-    private AppConfig appConfig;
 
     private TicketDao ticketDao;
 
     @BeforeEach
     void setUp() {
-        ticketDao = new TicketDaoCsv(ticketConfig, appConfig);
-        when(ticketConfig.getResourceName()).thenReturn("questions");
-        when(ticketConfig.getResourceExtension()).thenReturn("csv");
-        when(appConfig.getLocale()).thenReturn(Locale.ENGLISH);
+        ticketDao = new TicketDaoCsv(ticketConfig);
+        when(ticketConfig.getResourceName()).thenReturn("/questions_en.csv");
     }
 
     @Test
@@ -66,7 +60,7 @@ class TicketDaoCsvTest {
     @Test
     @DisplayName("бросает TicketNotFoundException если отсутствует")
     public void getTicketTicketNotFoundException() {
-        when(ticketConfig.getResourceName()).thenReturn("fakeQuestions.csv");
+        when(ticketConfig.getResourceName()).thenReturn("/fakeQuestions_en.csv");
         assertThrows(TicketNotFoundException.class, () -> ticketDao.getTicket());
     }
 }
