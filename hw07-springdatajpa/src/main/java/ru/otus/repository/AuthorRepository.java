f95e272b10d38e6644d82b11a4a285ee40d4e5eb
kireevaa85 <kireevaa85@gmail.com>
2020-10-12 15:24:27 +0300

    hw07 Refactor AuthorDao and GenreDao to Repository

--- hw07-springdatajpa/src/main/java/ru/otus/repository/AuthorRepository.java
+++ hw07-springdatajpa/src/main/java/ru/otus/repository/AuthorRepository.java
@@ -3,13 +3,6 @@ package ru.otus.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.domain.Author;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-    Optional<Author> findById(Long id);
-
-    List<Author> findAll();
 
 }
--- hw07-springdatajpa/src/main/java/ru/otus/repository/GenreRepository.java
+++ hw07-springdatajpa/src/main/java/ru/otus/repository/GenreRepository.java
@@ -3,13 +3,6 @@ package ru.otus.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.domain.Genre;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface GenreRepository extends JpaRepository<Genre, Long> {
-
-    Optional<Genre> findById(Long id);
-
-    List<Genre> findAll();
 
 }
